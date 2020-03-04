34a82ed10ba5fc8f1a09f0eacb0375f6e4a7c2b1
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-04 18:18:08 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,22 @@
+class Backer
+  
+  attr_reader :name 
+  
+  attr_accessor :backed_projects
+  
+  def initialize(name)
+    @name = name 
+    @backed_projects = []
+  end
+  
+  def project 
+    Project.all.select{|project| project.backer == self}
+  end
+  
+  def back_project(project)
+    @backed_projects << project
+    project.add_backer(self)
+  end
+  
+  
+end
